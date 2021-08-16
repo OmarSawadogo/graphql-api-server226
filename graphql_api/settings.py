e0f2303bde054d5e812236e123d280261c1f4976
Omar Sawadogo <sawomar226@gmail.com>
2021-08-16 00:29:17 +0000

    graphql_api_server226
    commit

--- graphql_api/settings.py
+++ graphql_api/settings.py
@@ -22,8 +22,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-#SECRET_KEY = 'django-insecure-^h5lw5-6+i&78k5jxx9jwcc8dqwag-b_$k%*afe=3#3tzs(al&'
-SECRET_KEY = os.environ.get("SECRET_KEY")
+SECRET_KEY = 'django-insecure-^h5lw5-6+i&78k5jxx9jwcc8dqwag-b_$k%*afe=3#3tzs(al&'
+#SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
